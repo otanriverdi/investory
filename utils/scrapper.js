cd56ba5e8e62aebdf7afaf45d6c6af9e930fe191
arindamaluni <aaluni@gmail.com>
2020-11-27 21:35:43 +0000

    style(prettier formatting): prettier
    prettier

--- utils/scrapper.js
+++ utils/scrapper.js
@@ -7,19 +7,16 @@ const BASE_URL_PROD = 'https://cloud.iexapis.com/stable/ref-data'
 const token = 'TOKEN';
 
 const saveDataTofile = async (baseURL, path, API_KEY, fileName) => {
-  try {
     const URL = `${baseURL}/${path}?token=${API_KEY}`;
     console.log(URL)
     const res = await fetch(URL);
     const data = await res.json();
-    console.log('Entries Found:' + data.length);
     if (path.includes('fx')) {
       writeCSV(fileName, data.currencies, 'stock', fileName);
-    } else
+    } else {
       writeCSV(fileName, data, 'stock', fileName);
-  } catch (err) {
-    console.log('Error : ' + err.message);
-  }
+    }
+
 }
 
 const writeCSV = (fileName, jsonData, type) => {
