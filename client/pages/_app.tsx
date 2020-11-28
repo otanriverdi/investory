eda5f76600f7365ff4c8d1f6565b4b0728c8cee9
Jack Doyle <jackdoyle@live.com>
2020-11-28 12:22:25 +0000

    feat(client): added apollo client to client

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,21 @@
 import {ChakraProvider, CSSReset} from '@chakra-ui/react';
 import type {AppProps} from 'next/app';
 import React from 'react';
+import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 
 const MyApp: React.FC<AppProps> = ({Component, pageProps}: AppProps) => {
+  const client = new ApolloClient({
+    uri: 'http://localhost:4000/',
+    cache: new InMemoryCache(),
+  });
+
   return (
-    <ChakraProvider>
-      <CSSReset />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <CSSReset />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   );
 };
 
