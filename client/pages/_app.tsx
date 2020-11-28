dc0089692188aed994d689faeb7aa5fc0b7a7647
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2020-11-28 15:43:18 +0300

    feat(client): adds the summary component without the graph

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,7 @@
 import {ChakraProvider, CSSReset} from '@chakra-ui/react';
 import type {AppProps} from 'next/app';
 import React from 'react';
-import Layout from '../components/layout/layout';
+import Layout from '../components/layout';
 
 const MyApp: React.FC<AppProps> = ({Component, pageProps}: AppProps) => {
   return (
