ccb1534de5707209c77432006536d33fd2ffc766
Jack Doyle <jackdoyle@live.com>
2020-11-28 07:32:33 +0000

    feat(server): apollo server and graphql implemented

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,17 @@
 import 'reflect-metadata'; // required for typeorm
 import {createConnection} from 'typeorm';
+import {ApolloServer} from 'apollo-server';
+import {typeDefs} from './typeDefs';
+import {resolvers} from './resolvers';
 
 createConnection()
   .then(async connection => {
     // TODO add connection to graphql context.
 
     // SERVER LOGIC SHOULD BE HERE TO MAKE SURE IT RUNS AFTER THE CONNECTION
-
-    console.log('Hello, world!');
+    const server = new ApolloServer({typeDefs, resolvers});
+    server.listen().then(({url}) => {
+      console.log(`🚀 Apollo server ready at ${url}`); // eslint-disable-line no-console
+    });
   })
   .catch(error => console.error(error));
