01b8cb9701e393305a4eb8e18026368408054592
Ozgur Tanriverdi <ozgur.tanriverdi94@gmail.com>
2020-12-08 20:39:08 +0300

    fix(client): fix and refactor for the form

--- client/components/positions/form.tsx
+++ client/components/positions/form.tsx
@@ -1,8 +1,6 @@
-import {CheckIcon, Search2Icon} from '@chakra-ui/icons';
+import {CheckIcon, CloseIcon, Search2Icon} from '@chakra-ui/icons';
 import {
   Box,
-  Button,
-  Divider,
   Flex,
   FormControl,
   FormLabel,
@@ -17,6 +15,10 @@ import {
   Radio,
   RadioGroup,
   Stack,
+  chakra,
+  useToast,
+  useColorMode,
+  IconButton,
 } from '@chakra-ui/react';
 import React, {useEffect, useState} from 'react';
 import DatePicker from 'react-datepicker';
@@ -28,15 +30,40 @@ import {
   useGetInstrumentsLazyQuery,
 } from '../../graphql/generated/graphql';
 
-const Form: React.FC = () => {
-  const [price, setPrice] = useState(0);
-  const [amount, setAmount] = useState(0);
+type InputProps = {
+  label: string;
+};
+
+const MyDatePicker = chakra(DatePicker);
+
+const FormInput: React.FC<InputProps> = ({children, label}) => (
+  <Flex direction="column" justify="space-between">
+    <FormLabel fontWeight="bold" ml={1}>
+      {label}
+    </FormLabel>
+    {children}
+  </Flex>
+);
+
+type FormProps = {
+  onComplete?: () => unknown;
+};
+
+const Form: React.FC<FormProps> = ({
+  onComplete = () => {
+    return;
+  },
+}) => {
+  const [price, setPrice] = useState('0');
+  const [amount, setAmount] = useState('0');
   const [symbol, setSymbol] = useState('');
   const [posDate, setPosDate] = useState(new Date());
-  const [posType, setPosType] = useState('');
-  const [selectionDisplay, setSelectionDisplay] = useState('none');
+  const [posType, setPosType] = useState<PositionType>(PositionType.Buy);
   const [getInstruments, {data}] = useGetInstrumentsLazyQuery();
   const [createPosition] = useCreatePositionMutation();
+
+  const toast = useToast();
+  const {colorMode} = useColorMode();
 
   useEffect(() => {
     if (symbol.length > 1) {
@@ -52,161 +79,166 @@ const Form: React.FC = () => {
     await createPosition({
       variables: {
         fields: {
-          amount,
-          price,
+          amount: +amount,
+          price: +price,
           symbol,
           date: posDate,
+          type: posType,
         },
       },
       refetchQueries: ['getPositions'],
     });
+
+    toast({
+      title: 'Success!',
+      description: 'We created your position.',
+      status: 'success',
+      duration: 9000,
+      isClosable: true,
+      position: 'top',
+    });
+
     setSymbol('');
-    setPrice(0);
-    setAmount(0);
+    setPrice('0');
+    setAmount('0');
+
+    onComplete();
   }
 
   function handleSymbolSelect(instrument: Instrument) {
     setSymbol(instrument.symbol);
-    setPrice(instrument.price.current);
-    setSelectionDisplay('none');
+    setPrice(instrument.price ? instrument.price.current.toString() : '0');
   }
 
   return (
-    <FormControl>
-      <Flex
-        overflow="visible"
-        mb={2}
-        justify="space-between"
-        wrap="wrap"
-        align="center"
-      >
-        <Flex overflow="visible" direction="column" justify="space-between">
-          <FormLabel fontWeight="bold" pl={4}>
-            Symbol
-          </FormLabel>
-          <Box overflow="visible" position="relative">
-            <InputGroup>
-              <InputLeftElement pointerEvents="none">
-                <Search2Icon color="cyan.500" />
-              </InputLeftElement>
-              <Input
-                placeholder="Symbol"
-                onChange={e => {
-                  setSymbol(e.target.value);
-                  setSelectionDisplay('block');
-                }}
-                value={symbol}
-                w={200}
-              />
-            </InputGroup>
-            <Box
-              overflow="visible"
-              borderRadius="md"
-              borderWidth="1px"
-              display={selectionDisplay}
-              position="absolute"
-              w={200}
-              h={200}
-            >
-              {data &&
-                data.getInstruments.map(instrument => (
-                  <Box
-                    overflow="visible"
-                    key={instrument.symbol}
-                    pl={4}
-                    pr={2}
-                    // TODO: need onClick={handleMenu()} to handle setting symbol and price
-                    onClick={e => handleSymbolSelect(instrument)}
-                    _hover={{
-                      transform: 'translateY(-3px)',
-                      background: 'cyan.500',
-                    }}
-                  >
-                    {instrument.symbol}
-                  </Box>
-                ))}
+    <Box p={4} borderWidth="1px" borderRadius="md">
+      <FormControl>
+        <Flex justify="space-between" align="center">
+          <FormInput label="Symbol">
+            <Box position="relative">
+              <InputGroup>
+                <InputLeftElement pointerEvents="none">
+                  <Search2Icon color="cyan.300" />
+                </InputLeftElement>
+                <Input
+                  placeholder="Symbol"
+                  autoComplete="off"
+                  onChange={e => {
+                    if (e.target.value.length === 0) {
+                      setPrice('0');
+                    }
+
+                    setSymbol(e.target.value);
+                  }}
+                  value={symbol}
+                />
+              </InputGroup>
+              <Box
+                overflow="scroll"
+                zIndex={2}
+                backgroundColor={colorMode === 'light' ? 'white' : 'gray.500'}
+                borderRadius="md"
+                borderWidth="1px"
+                hidden={symbol.length < 2 || +price > 0}
+                position="absolute"
+                width="100%"
+                h={100}
+              >
+                {data &&
+                  data.getInstruments.map(instrument => (
+                    <Box
+                      key={instrument.symbol}
+                      pl={4}
+                      pr={2}
+                      onClick={() => handleSymbolSelect(instrument)}
+                      _hover={{
+                        background: 'cyan.300',
+                        color: 'black',
+                      }}
+                    >
+                      {instrument.symbol}
+                    </Box>
+                  ))}
+              </Box>
             </Box>
-          </Box>
+          </FormInput>
+
+          <FormInput label="Price ($)">
+            <NumberInput
+              defaultValue={15}
+              precision={2}
+              value={price}
+              onChange={num => setPrice(num)}
+            >
+              <NumberInputField />
+            </NumberInput>
+          </FormInput>
+
+          <FormInput label="Quantity">
+            <NumberInput
+              inputMode="decimal"
+              precision={8}
+              step={1}
+              value={amount}
+              onChange={num => setAmount(num)}
+            >
+              <NumberInputField />
+              <NumberInputStepper>
+                <NumberIncrementStepper />
+                <NumberDecrementStepper />
+              </NumberInputStepper>
+            </NumberInput>
+          </FormInput>
+
+          <FormInput label="Opening Date">
+            <Box borderRadius="md" borderWidth="1px" h={41} p={2}>
+              <MyDatePicker
+                zIndex="5"
+                background="inherit"
+                outLineStyle="none"
+                selected={posDate}
+                onChange={date => setPosDate(date)}
+              />
+            </Box>
+          </FormInput>
+
+          <FormInput label="Type">
+            <RadioGroup
+              value={posType}
+              defaultValue={PositionType.Buy}
+              borderWidth="1px"
+              p={2}
+              borderRadius="md"
+              onChange={type => setPosType(type as PositionType)}
+            >
+              <Stack direction="row">
+                <Radio value={PositionType.Buy}>Buy</Radio>
+                <Radio value={PositionType.Sell}>Sell</Radio>
+              </Stack>
+            </RadioGroup>
+          </FormInput>
         </Flex>
 
-        <Flex direction="column" justify="space-between">
-          <FormLabel fontWeight="bold" pl={4}>
-            Price
-          </FormLabel>
-          <NumberInput
-            defaultValue={15}
-            precision={2}
-            step={0.2}
-            value={price}
-            onChange={(_, num) => setPrice(num)}
-          >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
+        <Flex mt={4} mb={2} justify="flex-end">
+          <IconButton
+            aria-label="save position"
+            bg="red.300"
+            icon={<CloseIcon />}
+            onClick={onComplete}
+            mr={2}
+          />
+          <IconButton
+            aria-label="cancel position"
+            bg="cyan.300"
+            icon={<CheckIcon />}
+            onClick={handleSubmit}
+            disabled={
+              symbol.length < 3 || Number(price) <= 0 || Number(amount) <= 0
+            }
+          />
         </Flex>
-        <Flex direction="column" justify="space-between">
-          <FormLabel fontWeight="bold" pl={4}>
-            Date
-          </FormLabel>
-          <Box borderRadius="md" borderWidth="1px" h={41} p={2}>
-            <DatePicker
-              outLineStyle="none"
-              selected={posDate}
-              onChange={date => setPosDate(date)}
-            />
-          </Box>
-        </Flex>
-        <Flex direction="column">
-          <FormLabel fontWeight="bold" pl={4}>
-            Quantity
-          </FormLabel>
-          <NumberInput
-            defaultValue={200}
-            step={10}
-            value={amount}
-            onChange={(_, num) => setAmount(num)}
-          >
-            <NumberInputField />
-            <NumberInputStepper>
-              <NumberIncrementStepper />
-              <NumberDecrementStepper />
-            </NumberInputStepper>
-          </NumberInput>
-        </Flex>
-        <Flex direction="column">
-          <FormLabel fontWeight="bold" pl={4}>
-            Position
-          </FormLabel>
-          {/* // TODO: need to handle position type */}
-          <RadioGroup
-            value={posType}
-            defaultValue={PositionType.Buy}
-            borderWidth={1}
-            p={2}
-            borderRadius="md"
-          >
-            <Stack direction="row">
-              <Radio value="Buy">Buy</Radio>
-              <Radio value="Sell">Sell</Radio>
-            </Stack>
-          </RadioGroup>
-        </Flex>
-      </Flex>
-      <Divider mt={3} mb={3} />
-      <Flex justify="flex-end">
-        <Button
-          bg="cyan.500"
-          leftIcon={<CheckIcon />}
-          variant="solid"
-          onClick={handleSubmit}
-        >
-          Save
-        </Button>
-      </Flex>
-    </FormControl>
+      </FormControl>
+    </Box>
   );
 };
 
